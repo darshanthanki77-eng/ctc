46adee6d9a4918d454878c591867369836443fe2
Abuzar Munshi <zarwebcoders@gmail.com>
2026-07-20 15:02:05 +0530

    Trigger Vercel production deployment build v1.0.1

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+// CTC App - Production Route Setup
 import React from 'react';
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import Layout from './components/Layout';
