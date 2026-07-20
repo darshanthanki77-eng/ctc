b2956dd65facf2d83b9d2c3ea44dfca01b057e1c
Abuzar Munshi <zarwebcoders@gmail.com>
2026-07-20 17:12:18 +0530

    Add PublicLayout and update catch-all route redirect

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
 // CTC App - Production Route Setup
-import React from 'react';
+import React, { useEffect } from 'react';
 import { BrowserRouter, Routes, Route, Navigate, Outlet } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { setWalletAddress } from './redux/slices/authSlice';
+
 import Layout from './components/Layout';
 import Dashboard from './pages/Dashboard';
 import Products from './pages/Products';
@@ -18,9 +21,6 @@ import PackageHistory from './pages/PackageHistory';
 import PromotionalBonusHistory from './pages/PromotionalBonusHistory';
 import Notifications from './pages/Notifications';
 import Landing from './pages/Landing';
-import { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { setWalletAddress } from './redux/slices/authSlice';
 
 const ProtectedLayout = () => {
   const { user } = useSelector((state) => state.auth);
@@ -30,30 +30,42 @@ const ProtectedLayout = () => {
   return <Layout />;
 };
 
+// Add a public layout wrapper for landing/auth pages
+const PublicLayout = () => {
+  const { user } = useSelector((state) => state.auth);
+  // If user is already authenticated, redirect to dashboard
+  if (user) {
+    return <Navigate to="/dashboard" replace />;
+  }
+  return <Outlet />;
+};
+
 function App() {
   const dispatch = useDispatch();
 
   useEffect(() => {
     if (window.ethereum) {
-      window.ethereum.on('accountsChanged', (accounts) => {
-        if (accounts.length > 0) {
-          dispatch(setWalletAddress(accounts[0]));
-        } else {
-          dispatch(setWalletAddress(null));
-        }
-      });
-
-      // Check initial connection
-      window.ethereum.request({ method: 'eth_accounts' }).then((accounts) => {
+      const handleAccounts = (accounts) => {
         if (accounts && accounts.length > 0) {
           dispatch(setWalletAddress(accounts[0]));
         } else {
           dispatch(setWalletAddress(null));
         }
-      }).catch((err) => {
-        console.error(err);
-        dispatch(setWalletAddress(null));
-      });
+      };
+
+      window.ethereum.on('accountsChanged', handleAccounts);
+
+      // Check initial connection
+      window.ethereum.request({ method: 'eth_accounts' })
+        .then((accounts) => {
+          if (accounts && accounts.length > 0) {
+            dispatch(setWalletAddress(accounts[0]));
+          }
+        })
+        .catch((err) => {
+          console.error('Ethereum connection error:', err);
+          dispatch(setWalletAddress(null));
+        });
     } else {
       dispatch(setWalletAddress(null));
     }
@@ -62,11 +74,13 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        {/* Public Routes */}
-        <Route path="/" element={<Landing />} />
-        <Route path="/landing" element={<Landing />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
+        {/* Public Routes - Redirect to dashboard if already logged in */}
+        <Route element={<PublicLayout />}>
+          <Route path="/" element={<Landing />} />
+          <Route path="/landing" element={<Landing />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+        </Route>
 
         {/* Protected Dashboard Layout Routes */}
         <Route element={<ProtectedLayout />}>
@@ -110,8 +124,8 @@ function App() {
           <Route path="notifications" element={<Notifications />} />
         </Route>
 
-        {/* Fallback Catch-All */}
-        <Route path="*" element={<Navigate to="/dashboard" replace />} />
+        {/* Fallback Catch-All - Redirect to landing for unauthenticated, dashboard for authenticated */}
+        <Route path="*" element={<Navigate to="/landing" replace />} />
       </Routes>
     </BrowserRouter>
   );
